2e0080db492701065da9060d20aaa1a71eb0af29
bogialmasi <boglarkaalmasi@gmail.com>
2021-10-22 08:10:24 +0200

    first JSON

---
+++ src/main/java/hu/firstJSON/FirstJsonApplication.java
@@ -0,0 +1,13 @@
+package hu.firstJSON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJsonApplication.class, args);
+	}
+
+}
